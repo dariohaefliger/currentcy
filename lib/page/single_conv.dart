37bb8fa28d15d3ea37271a0f739dcb17d7f840f8
dariohaefliger <dario.haefliger@gmail.com>
2025-10-24 09:50:08 +0200

    small: comment on single_conv

--- lib/page/single_conv.dart
+++ lib/page/single_conv.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class Single extends StatelessWidget {
   const Single({super.key});
 
+  // This is the main page or Single Conversion Tab.
   @override
   Widget build(BuildContext context) => Scaffold(
     body: Container(
@@ -14,7 +15,7 @@ class Single extends StatelessWidget {
           color: Colors.black,
           fontWeight: FontWeight.bold,
         ),
-        )
-      )
+      ),
+    ),
   );
 }
